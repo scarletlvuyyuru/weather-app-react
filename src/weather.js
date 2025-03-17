0c523ddf9b261746f13494a78b98047739efeb61
scarletlvuyyuru <slvuyyuru@hotmail.com>
2025-03-17 15:00:42 -0400

    bug fix

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import axios from "axios";
 
-export default function Weather(props) {
+export default function weather(props) {
     const [temperature, setTemperature] = useState(null);
   
     function showTemperature(response) {
